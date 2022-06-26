57510c58d913471907322dca6e2248fb76a99ec2
muffin1000 <tohtani1028@gmail.com>
2022-06-26 12:42:09 +0900

    chenge all file

--- main.py
+++ main.py
@@ -1,59 +1,56 @@
 import json
 import requests
-from pydoc import cli
-from this import d
 import time
-from urllib import response
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 # ユーザーID, パスワードを指定
-user_id = 'your mail address'
-user_pass = 'your account password'
+mail = 'your mail'
+psw = 'your password'
 
-# chromedriverのPATHを指定
-driver_path = '/app/.chromedriver/bin/chromedriver'
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--remote-debugging-port=9222')
-driver = webdriver.Chrome(options=options, executable_path=driver_path)
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
 # URLを指定
-url = 'https://login.microsoftonline.com/common/oauth2/authorize?client_id=c9a559d2-7aab-4f13-a6ed-e7e9c52aec87&resource=c9a559d2-7aab-4f13-a6ed-e7e9c52aec87&response_type=code%20id_token&scope=openid%20profile&state=OpenIdConnect.AuthenticationProperties%3DeyJ2ZXJzaW9uIjoxLCJkYXRhIjp7IklkZW50aXR5UHJvdmlkZXIiOiJBVGJLMUNxZWFvODlmdkprZENFN2xjdW5ITzdyeFVXQW1teEU0NDNTQUdsc1lrM2RiV3V0UjBjRzEtRUc3OFNqMXRGaVh2Wnd1QXM0cDE5Mm95N0c1RHciLCIucmVkaXJlY3QiOiJodHRwczovL2Zvcm1zLm9mZmljZS5jb20vUGFnZXMvUmVzcG9uc2VQYWdlLmFzcHg_aWQ9TXBGOU9WZE50MFdKSWxsTmZqWV82X1pMemxnZ2UweEVwWHJRcEh5clVPOVVSVlJTVURjMldGRlFXVUpNVlU5Vk5EZFFSVlJJUWpCSVdTNHUmc2lkPTA1Y2QwM2U4LWEwZjktNDdhMi1hZjQ2LTljMWI5MjgxNjdiOCJ9fQ&response_mode=form_post&nonce=637800136823934510.OWZjYjE1OTUtNmZlMS00OWI1LWI1ZjEtMzE3MDhjYzYyMDA5N2YxNzVjYzQtNjU0My00ZjhmLWJjZDktMDcxYjY2MzU5ZDQ4&redirect_uri=https%3A%2F%2Fforms.office.com%2Flanding&msafed=0&x-client-SKU=ID_NET472&x-client-ver=6.14.1.0&sso_reload=true'
+url = 'https://login.microsoftonline.com/common/oauth2/authorize?client_id=c9a559d2-7aab-4f13-a6ed-e7e9c52aec87&resource=c9a559d2-7aab-4f13-a6ed-e7e9c52aec87&response_type=code%20id_token&scope=openid%20profile&state=OpenIdConnect.AuthenticationProperties%3DeyJ2ZXJzaW9uIjoxLCJkYXRhIjp7IklkZW50aXR5UHJvdmlkZXIiOiJBWjVjZXI4WDRWY0ZhMG1kLWo5YnFmazFtcllvLWo4M3M3NVBTZDJHalAzc1Z1Q3Qxc0s4UE5CeTF6TF9lRXpjcTFOX2hsUmxCSFMxc2JWMGN6eHVMZW8iLCIucmVkaXJlY3QiOiJodHRwczovL2Zvcm1zLm9mZmljZS5jb20vUGFnZXMvUmVzcG9uc2VQYWdlLmFzcHg_aWQ9TXBGOU9WZE50MFdKSWxsTmZqWV82X1pMemxnZ2UweEVwWHJRcEh5clVPOVVSVlJTVURjMldGRlFXVUpNVlU5Vk5EZFFSVlJJUWpCSVdTNHUmc2lkPWE5OWUwYWMyLTY5ZjctNGNhNS1hODE2LThlYTVlYjhkY2Y4YSJ9fQ&response_mode=form_post&nonce=637910755195240468.MzcwNzVjMmUtN2ZiMS00MjNhLTk1YjYtNTE2NTFmNTc5NzZkZDk5OThjMmMtZTc0MC00MGJjLTgzYjUtY2IxMDNjZDQ2MTBi&redirect_uri=https%3A%2F%2Fforms.office.com%2Flanding&msafed=0&x-client-SKU=ID_NET472&x-client-ver=6.15.1.0'
 # URLを開く
 driver.get(url)
 print('urlを開いた')
+time.sleep(5)
 # ユーザーIDとパスワードを入力
-element = driver.find_element_by_css_selector('#i0116')
-element.send_keys(user_id)
-time.sleep(3)
-element = driver.find_element_by_css_selector('#idSIButton9')
+element = driver.find_element_by_id("i0116")
+element.send_keys(mail)
+element = driver.find_element_by_id("idSIButton9")
 element.click()
-time.sleep(3)
 print('メールアドレスを入力した')
-element = driver.find_element_by_css_selector('#i0118')
-element.send_keys(user_pass)
 time.sleep(3)
-element = driver.find_element_by_css_selector('#idSIButton9')
+
+element = driver.find_element_by_id("i0118")
+element.send_keys(psw)
+element = driver.find_element_by_id("idSIButton9")
 element.click()
-time.sleep(5)
 print('パスワードを入力した')
+time.sleep(3)
+
 # ログイン情報を記録させるかどうか
-element = driver.find_element_by_css_selector('#idSIButton9')
+element = driver.find_element_by_id("idSIButton9")
 element.click()
-time.sleep(5)
 print('はいを押した')
+time.sleep(5)
 
 # フォームに入力
 element = driver.find_element_by_xpath(
-    '//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/label/input')
+    "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div/div/div[3]/div/div[2]/div/label/input")
 element.click()
 print('体温を選択')
 element = driver.find_element_by_xpath(
-    '//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[4]/div[1]/button/div')
+    "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[4]/div[1]/button/div")
 element.click()
 print('送信')
 
@@ -63,9 +60,7 @@ print('成功')
 
 # discordに通知するように設定する(webhook経由)
 
-wenhook_url = 'discordのwebhookのurl'
+wenhook_url = 'your webhook url'
 main_content = {'content': '検温は送信されました'}
 headers = {'Content-Type': 'application/json'}
-
-response = requests.post(
-    wenhook_url, data=json.dumps(main_content), headers=headers)
+requests.post(wenhook_url, data=json.dumps(main_content), headers=headers)
